c01833734da4a18806fd15d5a092f28a450e38a3
arsmohamed <arsmohamed@outlook.com>
2023-12-27 18:07:14 -0330

    reformating the componentspanel and changed the label and texting and added definition to the container text and button

--- frontend/src/AppComponents/ComponentsPanel/UserComponents/Container.js
+++ frontend/src/AppComponents/ComponentsPanel/UserComponents/Container.js
@@ -1,10 +1,19 @@
 import ClickableWrapper from "./ClickableWrapper";
 import styled from "styled-components";
 
+const DivContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const TitleStyle = styled.p`
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+`;
 const ContainerStyle = styled.div`
   background-color: #f39f5a;
   color: black;
-  width: 20vw;
+  width: 18vw;
   height: 15vh;
   padding: 10px;
   border: 1px solid #f39f5a;
@@ -25,7 +34,10 @@ const ContainerStyle = styled.div`
 export default function Container() {
   return (
     <ClickableWrapper>
-      <ContainerStyle> Container Section </ContainerStyle>
+      <DivContainer>
+        <TitleStyle>Div</TitleStyle>
+        <ContainerStyle> Container </ContainerStyle>
+      </DivContainer>
     </ClickableWrapper>
   );
 }
