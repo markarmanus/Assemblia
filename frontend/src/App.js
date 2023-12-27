d23d274e9fc19927bcc73e98fbf1af2fb17199ea
arsmohamed <arsmohamed@outlook.com>
2023-12-27 16:16:00 -0330

    changed the code in the app.js to the componentspanel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,19 @@ import { useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 import { MainContext } from "./Context/MainContext";
-
+import ComponentsPanel from "./AppComponents/ComponentsPanel/ComponentsPanel";
 import AttributesPanel from "./AppComponents/AttributesPanel/AttributesPanel";
 
 const MainPage = styled.div`
   height: 100vh;
   display: flex;
 `;
-const RightPanel = styled.div`
-  background-color: #0000aa;
-  width: 18vw;
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
 
 const EditPanel = styled.div`
   background-color: #fff;
   width: 64vw;
   overflow-y: scroll;
-  &::-webkit-scrollbar {
+  2 &::-webkit-scrollbar {
     display: none;
   }
 `;
@@ -38,7 +30,7 @@ function App() {
   return (
     <MainContext.Provider value={{ mainContext, setMainContext }}>
       <MainPage>
-        <RightPanel>Components Panel</RightPanel>
+        <ComponentsPanel />
         <EditPanel>
           <div id={0} className="App">
             {/* App.js->return->div->0->content */}
--- frontend/src/AppComponents/ComponentsPanel/ComponentsPanel.js
+++ frontend/src/AppComponents/ComponentsPanel/ComponentsPanel.js
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const RightPanel = styled.div`
+  background-color: #161d30;
+  color: white;
+  width: 18vw;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 function ComponentsPanel() {
-  return <div></div>;
+  return <RightPanel>Components Panel</RightPanel>;
 }
 
 export default ComponentsPanel;
