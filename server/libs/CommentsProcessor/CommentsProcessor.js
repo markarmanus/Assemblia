bf998b6493b979a7e8a10ab6e786a9a09d157801
arsmohamed <arsmohamed@outlook.com>
2023-12-27 14:32:13 -0330

    checked the filename or the id if they are null or not, and made sure that the return of each type matches a file outline

--- server/libs/CommentsProcessor/CommentsProcessor.js
+++ server/libs/CommentsProcessor/CommentsProcessor.js
@@ -7,29 +7,32 @@ const MODIFICATION_TYPES = {
 };
 
 const structureComment = (modificationType, fileName, componentType, id) => {
-    switch (modificationType) {
-        case MODIFICATION_TYPES.IMPORT:
-            return `/* IMPORT->${fileName} */`;
-        case MODIFICATION_TYPES.CONTENT:
-            return `/* ${fileName}->CONTENT->${id} */`;
-        case MODIFICATION_TYPES.STYLE:
-            return `/* ${fileName}->SYTLE->${id} */`;
-        case MODIFICATION_TYPES.ADD_COMPONENT:
-        case MODIFICATION_TYPES.DELETE_COMPONENT:
-            switch (componentType) {
-                case componentType.DIV:
-                    return `/* ${fileName}->DIV->${id} */`;
-                case componentType.CONTAINER:
-                    return `/* ${fileName}->CONTAINER->${id} */`;
-                case componentType.BUTTON:
-                    return `/* ${fileName}->BUTTON->${id} */`;
-                default:
-                    return `Unknown component type ${fileName}`;
-            }
-        default:
-            return `Unknown modification type in ${fileName}`;
-    }
-    // return `${fileName}->return`
+    if (fileName === null && id === null) {
+        return 'Filename or ID can not be null'
+    } else {
+        switch (modificationType) {
+            case MODIFICATION_TYPES.IMPORT:
+                return `/* ${fileName}->import */`;
+            case MODIFICATION_TYPES.CONTENT:
+                return `{/* ${fileName}->return->${id}->CONTENT */}`;
+            case MODIFICATION_TYPES.STYLE:
+                return `/* ${fileName}->SYTLE->${id} */`;
+            case MODIFICATION_TYPES.ADD_COMPONENT:
+            case MODIFICATION_TYPES.DELETE_COMPONENT:
+                switch (componentType) {
+                    case componentType.CONTAINER:
+                        return `{/* ${fileName}->return->${id}->CONTAINER */}`;
+                    case componentType.TEXT:
+                        return `{/* ${fileName}->return->${id}->TEXT */}`;
+                    case componentType.BUTTON:
+                        return `{/* ${fileName}->return->${id}->BUTTON */}`;
+                    default:
+                        return `Unknown component type ${fileName}`;
+                }
+            default:
+                return `Unknown modification type in ${fileName}`;
+        }
+    }// return `${fileName}->return`
 };
 
 module.exports = { MODIFICATION_TYPES, structureComment };
