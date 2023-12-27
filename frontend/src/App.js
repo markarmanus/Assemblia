ab0a050de77d19d17e77d36c08611e9b76848628
Fady Philips <fadyaphilips97@gmail.com>
2023-12-27 16:01:51 -0330

    created non-functional Attribute panel and installed styled components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
 import { useState } from "react";
+import styled from 'styled-components'
 import "./App.css";
 import { MainContext } from "./Context/MainContext";
 
 import AttributesPanel from "./AppComponents/AttributesPanel/AttributesPanel";
+
+const MainPage = styled.div`
+  height: 100vh;
+  display: flex;
+`
+const RightPanel = styled.div`
+  background-color: #0000aa;
+  width: 18vw;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const EditPanel = styled.div`
+  background-color: #fff;
+  width: 64vw;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
 
 function App() {
   const initialMainContext = {
@@ -14,10 +37,16 @@ function App() {
   const [mainContext, setMainContext] = useState(initialMainContext);
   return (
     <MainContext.Provider value={{ mainContext, setMainContext }}>
-      <div>
-        <div className="App">{/* App.js->return */}</div>;
-
-      </div>
+      <MainPage>
+        <RightPanel>
+          Components Panel
+        </RightPanel>
+        <EditPanel>
+          <div className="App">{/* App.js->return */}</div>
+          Main Component Area
+        </EditPanel>
+        <AttributesPanel />
+      </MainPage>
     </MainContext.Provider>
   );
 }
--- frontend/src/AppComponents/AttributesPanel/AttributesPanel.js
+++ frontend/src/AppComponents/AttributesPanel/AttributesPanel.js
@@ -1,7 +1,95 @@
+import styled from 'styled-components'
+
+const StyledAttributesPanel = styled.div`
+  background-color: #ddd;
+  width: 18vw;
+  border-left: 2px solid #888;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  overflow-y: scroll;
+    &::-webkit-scrollbar {
+    display: none;
+  }
+`
+const PanelTitle = styled.div`
+  font-size: 20px;
+  text-align: center;
+  border-bottom: 2px solid #888;
+  padding: 10px;
+`
+const AttributesList = styled.div`
+  background-color: red;
+  height: 100%
+
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+const PanelButtons = styled.div`
+  font-size: 20px;
+  border-top: 2px solid #888;
+  padding: 15px;
+
+  display: flex;
+  justify-content: space-evenly;
+`
+const DeleteButton = styled.button`
+  font-size: 18px;  
+  background-color: #cc0000;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 15px;
+  transition-duration: 0.2s;
+
+  &:hover {
+    background-color: #990000;
+    color: white;
+  };
+
+  &:active {
+    background-color: #aa0000;
+    transform: translateY(2px);
+  };
+`
+const ExportButton = styled.button`
+  font-size: 18px;  
+  background-color: #0000cc;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 15px;
+  transition-duration: 0.2s;
+
+  &:hover {
+    background-color: #000099;
+    color: white;
+  };
+
+  &:active {
+    background-color: #0000aa;
+    transform: translateY(2px);
+  };
+`
+
 function AttributesPanel() {
-  return <div>
-    Hello World
-  </div>;
+  return (
+    <StyledAttributesPanel>
+      <PanelTitle>
+        Attribute Panel
+      </PanelTitle>
+      <AttributesList>
+        All the shit
+      </AttributesList>
+      <PanelButtons>
+        <DeleteButton onClick={() => console.log("Component Deleted")}>Delete</DeleteButton>
+        <ExportButton onClick={() => console.log("Component Exported")}> Export</ExportButton>
+      </PanelButtons>
+    </StyledAttributesPanel >
+  )
 }
 
 export default AttributesPanel;
