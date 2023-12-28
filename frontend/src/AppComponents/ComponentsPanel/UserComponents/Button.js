6c2fb4c32eca7f64f08b5dd1d3d1627591c009e9
Mark Armanious <markarmanus@gmail.com>
2023-12-28 15:12:38 -0330

    fix button callin onClick when undefined

--- frontend/src/AppComponents/ComponentsPanel/UserComponents/Button.js
+++ frontend/src/AppComponents/ComponentsPanel/UserComponents/Button.js
@@ -6,7 +6,11 @@ import COMPONENT_TYPES from "../../../Constants/COMPONENT_TYPES";
 export default function Button(props) {
   const content = props.children || "Button";
   return (
-    <ClickableWrapper onClick={() => props.onClick(COMPONENT_TYPES.BUTTON, content)}>
+    <ClickableWrapper
+      onClick={() => {
+        if (props.onClick) props.onClick(COMPONENT_TYPES.BUTTON, content);
+      }}
+    >
       <button id="button">{content}</button>
     </ClickableWrapper>
   );
