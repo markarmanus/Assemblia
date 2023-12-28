f1b0a8622bbb9013a4b3e1e9ce287d334db0bbeb
Mark Armanious <markarmanus@gmail.com>
2023-12-28 14:13:57 -0330

    revert changes in App.js after merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,20 @@ import "./App.css";
 import { MainContext } from "./Context/MainContext";
 import AttributesPanel from "./AppComponents/AttributesPanel/AttributesPanel";
 import ComponentsPanel from "./AppComponents/ComponentsPanel/ComponentsPanel";
-import EditorPanel from "./AppComponents/EditorPanel/EditorPanel";
 /* App.js->import */
 
 const MainPage = styled.div`
   height: 100vh;
   display: flex;
+`;
+
+const EditPanelContainer = styled.div`
+  background-color: #fff;
+  width: 64vw;
+  overflow-y: scroll;
+  2 &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 function App() {
@@ -24,11 +32,11 @@ function App() {
     <MainContext.Provider value={{ mainContext, setMainContext }}>
       <MainPage>
         <ComponentsPanel />
-        <EditorPanel>
+        <EditPanelContainer>
           <div id={0} className="App">
             {/* App.js->return->div->0->content */}
           </div>
-        </EditorPanel>
+        </EditPanelContainer>
         <AttributesPanel />
       </MainPage>
     </MainContext.Provider>
